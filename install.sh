e4f67a906d43fc95d1e41b36ee05d10b28f4c82b
Teodor Heggelund <teodor+git@iterate.no>
2019-12-11 14:23:05 +0100

    Fix old install cleanup

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+set -e
+
 # Ensure we've got our folder and remove previous versions
 mkdir -p ~/.local/bin
-rm -r ~/.local/bin/hotload
+rm -f ~/.local/bin/hotload
 
 # Install latest version
 wget https://raw.githubusercontent.com/teodorlu/hotload/master/hotload.py -O ~/.local/bin/hotload
