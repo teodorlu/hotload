e58b5d4dfa1300d7f97d6af5cb9bb73d51446c35
Richqrd <richard.tingstad@gmail.com>
2022-07-03 14:14:57 +0200

    Test refactor: add mymath.py

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -e
 
-cat <<EOF >lib.py
-x = 3
-y = 4
-print("Result: {}".format(x*x + y*y))
+cat <<EOF >mymath.py
+def f(x,y):
+    return x*x + y*y
 EOF
-touch -t 200711121015 lib.py
+cat <<EOF >lib.py
+import mymath
+print("Result: {}".format(mymath.f(3,4)))
+EOF
+touch -t 200711121015 lib.py mymath.py
 
 echo lib.py | python3 -u ./hotload.py lib.py > output &
 pid="$!"
 
 sleep 0.1
-sed -i.bak "s/x = ./x = 4/" lib.py
+sed -i.bak 's/f(3,4)/f(4,4)/' lib.py
 sleep 0.1
 kill "$pid"
 
